5d26663cefebc4b0f809acba4190dd211f013916
arjunsuresh <arjunsuresh@users.noreply.github.com>
2025-02-12 15:28:26 +0000

    Deployed ba3c239 with MkDocs version: 1.6.1

---
+++ top_results/javascripts/config.js
@@ -0,0 +1,7 @@
+const results_version="v5.0";
+var repo_owner="mlcommons";
+var repo_branch="refs/heads/auto-update";
+var repo_name="mlperf_inference_unofficial_submissions_v5.0";
+const dbVersion ="12";
+const default_category ="datacenter";
+const default_division ="closed";
